a5516f2f54b32b19be3762cd3f4fc49363b6cbcb
Arsen <arsendada@mail.ru>
2022-12-02 00:19:05 +0300

    created function register to  views.py account application

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from django.contrib.auth import authenticate, login
 from .forms import LoginForm
 from django.contrib.auth.decorators import login_required
+from .forms import UserRegistraionForm
 
 
 # def user_login(request):
@@ -30,3 +31,22 @@ from django.contrib.auth.decorators import login_required
 def dashboard(request):
     return render(request, 'account/dashboard.html',
                   {'section': 'dashboard'})
+
+
+def register(request):
+    if request.method == 'POST':
+        user_form = UserRegistraionForm(request.POST)
+        if user_form.is_valid():
+            new_user = user_form.save(commit=False)
+            new_user.set_password(
+                user_form.cleaned_data['password']
+            )
+            new_user.save()
+            return render(request,
+                          'account/register_done.html',
+                          {'new_user': new_user})
+    else:
+        user_form = UserRegistraionForm()
+    return (request,
+            'account/register.html',
+            {'user_form': user_form})
