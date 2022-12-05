34aa9abfcd35127a6dfe1cc45547f3f142b9555d
Arsen <arsendada@mail.ru>
2022-12-05 22:03:55 +0300

    add password change and edit to dashboard

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -69,6 +69,6 @@ def edit(request):
         user_form = UserEditForm(instance=request.user)
         profile_form = ProfileEditForm(instance=request.user.profile)
     return render(request,
-                  'account/edti.html',
+                  'account/edit.html',
                   {'user_form': user_form,
                    'profile_form': profile_form})
