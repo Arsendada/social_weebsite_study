7a40e260a810210d78d2f259917911bc7640615f
Arsen <arsendada@mail.ru>
2022-12-13 02:38:52 +0300

    add debug_toolbar to main urls.py

--- bookmarks/bookmarks/urls.py
+++ bookmarks/bookmarks/urls.py
@@ -10,7 +10,8 @@ urlpatterns = [
     path('social-auth/',
          include('social_django.urls', namespace='social')),
     path('images/', include('images.urls',
-                            namespace='images'))
+                            namespace='images')),
+    path('__debug__/', include('debug_toolbar.urls')),
 ]
 
 if settings.DEBUG:
